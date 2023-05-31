775c8c5ff4e97061d072ffec37e94b35d78281e7
albanprvst1 <albanprvst@icloud.com>
2023-05-31 18:32:03 +0200

    react fonctions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function NamedFunction(props) {
+  return <p {...props}>Named function</p>;
 }
 
-export default App;
+const ArrowFunction = (props) => {
+  return <p {...props}>Arrow function</p>;
+};
+
+const ArrowFunctionWithImplicitReturn = (props) => (
+  <p {...props}>Arrow function with implicit return</p>
+);
+
+class ClassComponent extends React.Component {
+  render() {
+    return <p {...this.props}>Class Component</p>;
+  }
+}
+
+export default function App() {
+  return (
+    <React.Fragment>
+      <NamedFunction />
+      <ArrowFunction />
+      <ArrowFunctionWithImplicitReturn />
+      <ClassComponent />
+    </React.Fragment>
+  );
+}
